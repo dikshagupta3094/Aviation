3b33ee36d14fbba7d42ddba94715fb026c9718c3
Kashi2105 <kashisankhla2105@gmail.com>
2024-10-15 21:12:33 +0530

    kuch khaas nhi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./public/index.html",       // Main HTML file
-    "./src/components/*.js",       // All JS and HTML files in src folder
+    "./src/components/*.js", 
+    "./src/scripts/*.js",       // All JS and HTML files in src folder
     "./src/style/*.css"       // All CSS files in the styles folder (if needed)
 ],
   theme: {
