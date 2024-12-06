5247054bf86a69e7cd0794bc40eeb02ac6ab85e3
dikshagupta3094 <dikshagpragya@gmail.com>
2024-12-06 20:29:27 +0530

    add more faculties

--- src/scripts/faculties.js
+++ src/scripts/faculties.js
@@ -1,20 +1,4 @@
-let data = [
-  {
-    name: "Diksha Bhatia",
-    designation: "Marketing & Business Head",
-    image: "../src/assest/Faculties/diksha_bhatia.jpg",
-    qualification:"MBA(APR), pursuing NET",
-    Teaching_Experience:"10yrs",
-    specialization:"MarComm, Management"
-  },
-  {
-    name: "Khushbu Gupta ",
-    designation: "Assistant Professor ",
-    image: "../src/assest/Faculties/Khushbu_Gupta.jpg",
-    qualification:"MBA",
-    Teaching_Experience:"5yrs",
-    specialization:"HR"
-  },
+let data = [ 
   {
     name: "Ratan Ambhore",
     designation: "Head of ground operations",
@@ -24,12 +8,12 @@ let data = [
     specialization:"Tourism, Aviation Management"
   },
   {
-    name: "Anshul Kumar Jain",
-    designation: "Faculty",
-    image: "../src/assest/Faculties/Anshul_jain.jpg",
-    qualification:"PhD ( Pursuing), UGC NET COMMERCE",
-    Teaching_Experience:"3yrs",
-    specialization:"COMMERCE"
+    name: "Piyush Verma",
+    designation: "SR.MANAGER- AIR CARGO FOR VISTARA AIRLINE",
+    image: "../src/assest/Faculties/Piyush_Verma.jpg",
+    qualification:"MBA - INTERNATIONAL BUSINESS/ SUPPLY CHAIN MANAGEMENT",
+    Teaching_Experience:"11yrs",
+    specialization:"AVIATION,AIR CARGO, LOGISTICS, TRANSPORTATION, WAREHOUSING,INVENTTORY ,PURCHASE,EXPORT-IMPORT DOCUMENTATION, DISTRIBUTION,PRODUCTION PLANNING, E COMMERCE LOGISTICS,RAIL,ROAD,SEA FREIGHT MANAGEMENT,CUSTOMS,CHA,IATA ,DANGEROUS GOODS MGT, CUSTODIAN,INTERNATIONAL CARGO TERMINAL,ICD,INTERNATIONL FREIGHT,INCOTERMS"
   },
   {
     name: "Alok Anand Jha",
@@ -40,12 +24,57 @@ let data = [
     specialization:"Management, Marketing, OB, Tax, SPSS, Research"
   },
   {
-    name: "Piyush Verma",
-    designation: "SR.MANAGER- AIR CARGO FOR VISTARA AIRLINE",
-    image: "../src/assest/Faculties/Piyush_Verma.jpg",
-    qualification:"MBA - INTERNATIONAL BUSINESS/ SUPPLY CHAIN MANAGEMENT",
-    Teaching_Experience:"11yrs",
-    specialization:"AVIATION,AIR CARGO, LOGISTICS, TRANSPORTATION, WAREHOUSING,INVENTTORY ,PURCHASE,EXPORT-IMPORT DOCUMENTATION, DISTRIBUTION,PRODUCTION PLANNING, E COMMERCE LOGISTICS,RAIL,ROAD,SEA FREIGHT MANAGEMENT,CUSTOMS,CHA,IATA ,DANGEROUS GOODS MGT, CUSTODIAN,INTERNATIONAL CARGO TERMINAL,ICD,INTERNATIONL FREIGHT,INCOTERMS"
+    name: "Khushbu Gupta ",
+    designation: "Assistant Professor ",
+    image: "../src/assest/Faculties/Khushbu_Gupta.jpg",
+    qualification:"MBA",
+    Teaching_Experience:"5yrs",
+    specialization:"HR"
+  },
+ 
+  {
+    name: "Anshul Kumar Jain",
+    designation: "Faculty",
+    image: "../src/assest/Faculties/Anshul_jain.jpg",
+    qualification:"PhD ( Pursuing), UGC NET COMMERCE",
+    Teaching_Experience:"3yrs",
+    specialization:"COMMERCE"
+  },
+  {
+    name: "Diksha Bhatia",
+    designation: "Marketing & Business Head",
+    image: "../src/assest/Faculties/diksha_bhatia.jpg",
+    qualification:"MBA(APR), pursuing NET",
+    Teaching_Experience:"10yrs",
+    specialization:"MarComm, Management"
+  },
+
+  {
+    name: "Dr. Kartika Mehta",
+    designation: "Visiting Faculty",
+    image: "../src/assest/Faculties/Dr.Kartika_Gupta_Mehta.bmp",
+    qualification:"M. Com., B. Ed, M. Phil, PhD (Commerce)",
+    Teaching_Experience:"10yrs",
+    specialization:"Accounts and Business statistics"
+  },
+  
+  
+  {
+    name: "Dr.Shruti Maheshwari",
+    designation: "Visiting Faculty",
+    image: "../src/assest/Faculties/Dr.Shruti_P_Maheshwari.jpg",
+    qualification:"Phd-Management",
+    Teaching_Experience:"14yrs",
+    specialization:"Management, Skill enhancement, Personality development"
+  },
+
+  {
+    name: "Dr. Bharti  Arya",
+    designation: "Visiting Faculty",
+    image: "../src/assest/Faculties/Dr.Bharti_arya.jpg",
+    qualification:"Ph.D, MBA,M.Com,B.Ed",
+    Teaching_Experience:"9yrs",
+    specialization:"Finance, Accounts, Taxation"
   },
 ];
 
@@ -65,11 +94,11 @@ function getData() {
   for (let i = 0; i < data.length; i++) {
     const card = document.createElement("div");
     card.className =
-      "flex flex-col items-center justify-center w-[250px] md:my-5 my-5 mx-5 p-5 rounded-lg bg-[rgb(249,249,249)] transition ease-in-out delay-150 hover:-translate-y-1 duration-300 cursor-pointer";
+      "flex flex-col items-center justify-center w-[250px] md:my-5 my-5 mx-5 p-5 rounded-lg bg-blue-800 transition ease-in-out delay-150 hover:-translate-y-1 duration-300 cursor-pointer text-white font-semibold";
     card.innerHTML = `
       <img src=${data[i].image} class="h-[200px] w-[200px] rounded-lg mt-5" alt="Image" />
-      <p class="p-[1px] cursor-pointer hover:text-purple-600 font-semibold">${data[i].name}</p>
-      <p class="p-[1px] cursor-pointer hover:text-purple-600 font-semibold">${data[i].designation}</p>
+      <p class="p-[1px] cursor-pointer hover:text-gray-300 font-semibold">${data[i].name}</p>
+      <p class="p-[1px] cursor-pointer hover:text-gray-300 font-semibold">${data[i].designation}</p>
     `;
     // Add click event listener for popup
     card.addEventListener("click", () => showPopup(data[i]));
